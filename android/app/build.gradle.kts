75c6f3a768e6525e3e426f3dcf0461a60eb91879
angegevela <202280079@psu.palawan.edu.ph>
2025-11-29 01:04:19 +0800

    Add all missing files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.threadhub_system"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
